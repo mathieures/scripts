7ffd6842e9c0caa3b47ee72a846ef69aba1f172e
mathieures <mathieuresseguier@gmail.com>
2020-09-26 17:44:27 +0200

    added say.bat and rewrote Usages

--- hardlinks.bat
+++ hardlinks.bat
@@ -1,3 +1,4 @@
+@echo off
 if "%1"=="" goto :usage
 if "%3"=="" goto :usage
 cd %1
@@ -5,8 +6,8 @@ for %%F in (*) do mklink /h %2/%%F %%F
 goto :eof
 
 :usage
-@echo Creates hardlinks for every file in the source directory into the target directory
-@echo Usage:
-@echo 	hardlinks source_dir target_dir
+echo Creates hardlinks for every file in the source directory into the target directory
+echo Usage:
+echo 	hardlinks source_dir target_dir
 echo;
 pause
