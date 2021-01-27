9711b709fc76e575b05551d54ea713ec041e9fe0
mathieures <mathieuresseguier@gmail.com>
2021-01-27 14:06:26 +0100

    added usage help

--- admin.bat
+++ admin.bat
@@ -1,1 +1,14 @@
-@powershell -Command "Start-Process %* -Verb RunAs"
+@echo off
+
+if "%*"=="" goto :usage
+rem if there is no argument, show usage and end
+
+powershell -Command "Start-Process %* -Verb RunAs"
+
+
+:usage
+echo This script starts the given process with administrator privileges.
+echo  Usage:
+echo  %~nx0 ^<process^>
+echo;
+pause
