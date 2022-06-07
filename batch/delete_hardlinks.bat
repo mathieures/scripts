4c1767ff62eb2d947a972afdfdcb09ade309bbd2
mathieures <71087433+mathieures@users.noreply.github.com>
2022-06-07 03:34:39 +0200

    Script to delete every hardlinks of a file

---
+++ batch/delete_hardlinks.bat
@@ -0,0 +1,47 @@
+@echo off
+setlocal
+
+if "%*"=="" (
+    echo Usage: delete_hardlinks.bat [file_1] [file_2] [...]
+    echo    OR: Drag files onto the .bat script in the explorer
+    exit /b
+)
+
+rem For each file in the arguments
+for %%f in (%*) do (
+    echo Fichier : %%f
+    rem Loop through its hardlinks
+    for /f "usebackq tokens=*" %%h in (`fsutil hardlink list %%f`) DO (
+        echo;  Hardlink : %%h
+    )
+)
+
+echo;
+rem Note: there is a space at the end of the next line
+set /p answer=Delete all these files? ([y]es) 
+
+rem Logical OR
+if "%answer%" == "y" goto delete
+if "%answer%" == "yes" goto delete
+
+goto cancel
+
+:delete
+rem For each file in the arguments
+for %%f in (%*) do (
+    rem Loop through its hardlinks
+    for /f "usebackq tokens=*" %%h in (`fsutil hardlink list %%f`) DO (
+        echo Deleting: %%h
+        del "%%h"
+    )
+)
+goto end
+
+:cancel
+echo;Cancelled
+goto end
+
+
+:end
+echo;
+pause
