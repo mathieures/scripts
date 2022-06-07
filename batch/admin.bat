5039d0d78dd9304d68e3a6aa23f6831f9f7d318b
mathieures <71087433+mathieures@users.noreply.github.com>
2022-06-07 03:39:00 +0200

    Fixed an unnecessary echo

--- batch/admin.bat
+++ batch/admin.bat
@@ -4,11 +4,14 @@ if "%*"=="" goto :usage
 rem if there is no argument, show usage and end
 
 powershell -Command "Start-Process %* -Verb RunAs"
+goto end
 
 
 :usage
 echo This script starts the given process with administrator privileges.
 echo  Usage:
 echo  %~nx0 ^<process^>
+
+:end
 echo;
 pause
