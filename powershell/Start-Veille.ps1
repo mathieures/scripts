d90c118c2d58b77493951a5c21e7720507eb82f6
mathieures <71087433+mathieures@users.noreply.github.com>
2022-06-07 03:38:25 +0200

    Added comment

--- powershell/Start-Veille.ps1
+++ powershell/Start-Veille.ps1
@@ -4,7 +4,11 @@ function Start-Veille {
         [Alias('T')]
             [int]$Seconds = 5
     )
-    if(Start-Timer -Seconds $Seconds -Activity 'Mise en veille' -ShowProgressBar -C) { return }
+    if(Start-Timer -Seconds $Seconds -Activity 'Mise en veille' -ShowProgressBar) { return }
+    <#
+    Note: throws a terminating error if cancelled, but if the error handling behavior is changed,
+    we better prevent the script from executing the rest by checking if it returned wrongly.
+    #>
 
     Add-Type -AssemblyName System.Windows.Forms
     $PowerState = [System.Windows.Forms.PowerState]::Suspend;
