8eaa7453de69a1d335fe786efee51cd0c33471d6
mathieures <71087433+mathieures@users.noreply.github.com>
2022-06-07 03:45:48 +0200

    Updated the script

--- powershell/New-Link.ps1
+++ powershell/New-Link.ps1
@@ -1,12 +1,24 @@
 function New-Link {
-    # Create a link (symbolic by default) easier
+    # Create a link (symbolic by default) the easy way
     param(
         [ValidateSet('SymbolicLink','HardLink')]
             $Type = 'SymbolicLink',
-        [Parameter(Mandatory)]
-            [string]$Target, # The target, where the link should link to
-        [Parameter(Mandatory)]
-            [string]$Name # The name of the newly created link
+        [Parameter(Mandatory, Position=0)]
+            [string]$Name, # The name of the newly created link
+        [Parameter(Mandatory, Position=1)]
+            [string]$Target # The target, where the link should point
     )
-    New-Item -Path $Name -ItemType $Type -Value $Target
+    $TargetFile = Get-Item $Target
+    # If it exists, it's either a directory or it shouldn't be the name
+    if (Test-Path $Name)
+    {
+        if (Select-Object -InputObject (Get-Item $Name) -Property PsIsContainer)
+        {
+            $FilePath = $File.FullName + $TargetFile.Name
+        }
+    }
+    else { $FilePath = $Name }
+
+    # If the file already exists, it throws an error by itself
+    New-Item -Path $FilePath -ItemType $Type -Value $TargetFile.FullName
 }
