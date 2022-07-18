74f4cd75a3a0bc281dbb703868280736b24e7931
mathieures <71087433+mathieures@users.noreply.github.com>
2022-07-18 12:01:31 +0200

    Added help command and help message on first start

--- README.md
+++ README.md
@@ -7,7 +7,7 @@ c_kill.bat				| Provides an easier way to kill processes than the taskkill comma
 c_restart.bat			| Provides an easy way to restart certain processes from the command line
 gbash.bat				| Starts Git Bash if it is installed in the default location
 ipython.bat				| Starts IPython without having to specify `python -m IPython`
-auto_winget_upgrade.bat	| Upgrade automatically packages based on a list with `winget`
+auto_winget_upgrade.bat	| Upgrade packages automatically with `winget` based on a list
 New-Link.ps1			| Create a link (symbolic by default) the easy way
 Set-SplitLocation.ps1	| Change the directory to the one of the file
 Start-Timer.ps1			| A timer (5s by default) cancellable by pressing any key. A sound is played at the end.
--- auto_winget_upgrade.bat/auto_winget_upgrade.bat
+++ auto_winget_upgrade.bat/auto_winget_upgrade.bat
@@ -4,11 +4,19 @@ title Auto Winget Upgrade
 
 set packages_file=%LOCALAPPDATA%\auto_winget_upgrade_packages.txt
 rem Create empty file if it doesn't exist
-if not exist %packages_file% ( fsutil file createnew %packages_file% 0 )
+if not exist %packages_file% (
+	rem Note: fsutil displays a message for file creation
+	fsutil file createnew %packages_file% 0
+	echo To add packages, use:
+	echo %~nx0 add ^<package1^> [package2] [...]
+	goto end
+)
 
 if "%~1"=="add" goto adding_loop
 if "%~1"=="list" goto list_packages
 if "%~1"=="edit" goto edit_file
+if "%~1"=="help" goto show_help
+if "%~1"=="?" goto show_help
 
 rem No argument
 :upgrading
@@ -55,6 +63,20 @@ goto end
 :edit_file
 rem Open the file for edit
 notepad %packages_file%
+goto end
+
+
+:show_help
+echo Automatically upgrade packages from a list of packages ID's with winget
+echo  Usage:
+echo   - %~nx0 add ^<package1^> [package2] [...]
+echo     -^> To add packages to the list
+echo   - %~nx0 list
+echo     -^> To list the packages in the packages list:
+echo   - %~nx0 edit
+echo     -^> To open the packages list in notepad:
+echo;
+pause
 goto end
 
 
