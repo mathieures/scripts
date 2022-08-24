e250fc72d6e3bc6b74afbb49e10e75bd58f9d263
mathieures <71087433+mathieures@users.noreply.github.com>
2022-08-24 16:12:24 +0200

    Added description and example

--- dospath2unix.sh/dospath2unix.sh
+++ dospath2unix.sh/dospath2unix.sh
@@ -1,1 +1,3 @@
 echo "$*" | sed 's;\\;/;g' | sed -r 's;([A-Z]):;/mnt/\l\1;'
+# Convert backslashes to slashes, and add '/mnt/[lowercase disk letter]' at the beginning
+# e.g.: C:\Users\johndoe -> /mnt/c/Users/johndoe
