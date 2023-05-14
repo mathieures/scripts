a544de19d9f31636300c325b54d41551d491b934
mathieures <71087433+mathieures@users.noreply.github.com>
2023-05-14 14:09:37 +0200

    Renamed `Set-SplitLocation()` to `Set-LocationToFilePath()`

--- Set-LocationToFilePath.ps1/Set-LocationToFilePath.ps1
+++ Set-LocationToFilePath.ps1/Set-LocationToFilePath.ps1
@@ -1,4 +1,4 @@
-function Set-SplitLocation($Path) {
+function Set-LocationToFilePath($Path) {
     # Change the directory to the one of the file. An alias like 'cdf' could be useful.
     Set-Location (Split-Path $Path)
 }
