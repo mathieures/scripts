5ab67991b4c12fe4f03016625e7447efaded210e
mathieures <71087433+mathieures@users.noreply.github.com>
2024-01-27 19:00:30 +0100

    Improved help and set indentation to spaces

--- auto_winget_upgrade.bat/auto_winget_upgrade.bat
+++ auto_winget_upgrade.bat/auto_winget_upgrade.bat
@@ -3,9 +3,9 @@
 rem Change the window title safely
 rem If the flag is not defined, start the script in a child
 if not defined awu_child_process (
-	set awu_child_process=1
-	cmd /c %0 %*
-	exit /b
+    set awu_child_process=1
+    cmd /c %0 %*
+    exit /b
 )
 
 title Auto Winget Upgrade
@@ -15,11 +15,11 @@ set temp_file=%tmp%\auto_winget_upgrade_temp_file.txt
 
 rem Create empty file if it doesn't exist
 if not exist %packages_file% (
-	rem Note: fsutil displays a message for file creation
-	fsutil file createnew %packages_file% 0
-	echo To add packages, use:
-	echo %~nx0 add ^<package_id_1^> [package_id_2 ...]
-	exit /b
+    rem Note: fsutil displays a message for file creation
+    fsutil file createnew %packages_file% 0
+    echo To add packages, use:
+    echo %~nx0 add ^<package_id^> [...]
+    exit /b
 )
 
 if [%~1] == [add] goto add_command
@@ -37,14 +37,14 @@ rem List upgradable packages in a file
 
 rem Run the winget upgrade command for packages in the file
 for /f %%A in (%packages_file%) do (
-	rem If the package is found in the upgradable packages
-	>NUL find /i "%%A" %temp_file%
-	if not ERRORLEVEL 1 (
-		echo - Upgrading %%A
-		winget upgrade %%A
-	) else (
-		echo x No upgrade for %%A
-	)
+    rem If the package is found in the upgradable packages
+    >NUL find /i "%%A" %temp_file%
+    if not ERRORLEVEL 1 (
+        echo - Upgrading %%A
+        winget upgrade %%A
+    ) else (
+        echo x No upgrade for %%A
+    )
 )
 exit /b
 
@@ -54,19 +54,19 @@ rem Commands
 :add_command
 rem If there is only one argument ('add')
 if [%~2] == [] (
-	call :error_no_argument
-	exit /b 1
+    call :error_no_argument
+    exit /b 1
 )
 :adding_loop
 rem Remove the first argument each iteration
 shift
 rem Break condition
 if [%~1] == [] (
-	goto adding_loop_end
+    goto adding_loop_end
 ) else (
-	echo Adding %1
-	>>%packages_file% echo %1
-	goto adding_loop
+    echo Adding %1
+    >>%packages_file% echo %1
+    goto adding_loop
 )
 :adding_loop_end
 exit /b
@@ -75,29 +75,29 @@ exit /b
 :remove_command
 rem If there is only one argument ('remove')
 if [%~2] == [] (
-	call :error_no_argument
-	exit /b 1
+    call :error_no_argument
+    exit /b 1
 )
 :removing_loop
 rem Remove the first argument each iteration
 shift
 rem Break condition
 if [%~1] == [] (
-	goto removing_loop_end
+    goto removing_loop_end
 ) else (
-	rem Search for the package in the file
-	>NUL findstr /i /x %~1 %packages_file%
-	if ERRORLEVEL 1 (
-		echo Package '%~1' not found.
-	) else (
-		echo Removing '%~1'
-		rem Output all non matching lines in the temp file
-		>%temp_file% findstr /v /i /x %~1 %packages_file%
-		rem Overwrite the packages file with the temp file
-		>NUL move /y %temp_file% %packages_file%
-	)
+    rem Search for the package in the file
+    >NUL findstr /i /x %~1 %packages_file%
+    if ERRORLEVEL 1 (
+        echo Package '%~1' not found.
+    ) else (
+        echo Removing '%~1'
+        rem Output all non matching lines in the temp file
+        >%temp_file% findstr /v /i /x %~1 %packages_file%
+        rem Overwrite the packages file with the temp file
+        >NUL move /y %temp_file% %packages_file%
+    )
 
-	goto removing_loop
+    goto removing_loop
 )
 :removing_loop_end
 exit /b
@@ -106,7 +106,7 @@ exit /b
 :list_command
 rem List packages in the file
 for /f %%A in (%packages_file%) do (
-	echo %%A
+    echo %%A
 )
 exit /b
 
@@ -123,11 +123,11 @@ echo     Automatically upgrade packages from a list of packages ID's with winget
 echo     Tip: put a shortcut in %APPDATA%\Microsoft\Windows\Start Menu\Programs\Startup
 echo;
 echo     Commands:
-echo       add package_id [package_id ...]		Add given packages to the list
-echo       remove package_id [package_id ...]	Remove given packages from the list
-echo       list                           		List the packages in the packages list
-echo       edit                           		Open the packages list in notepad.exe
-echo       help^|?                        		Display this help
+echo       add package_id [...]     Add given packages to the list
+echo       remove package_id [...]  Remove given packages from the list
+echo       list                     List the packages in the packages list
+echo       edit                     Open the packages list in notepad.exe
+echo       help^|?                   Display this help
 echo;
 pause
 exit /b
